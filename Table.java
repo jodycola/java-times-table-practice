319c5dba12ed6548a11120b117d1ceb3a742e037
Kody Samaroo <github email address>
2021-06-19 17:15:01 -0400

    added notes

--- Table.java
+++ Table.java
@@ -2,6 +2,7 @@ public class Table
 {
     public static void main(String[] args)
     {
+        // Create arrays for each number in the table
         int[] ones = {1,2,3,4,5,6,7,8,9,10};
         int[] twos = {2,4,6,8,10,12,14,16,18,20};
         int[] threes = {3,6,9,12,15,18,21,24,27,30};
@@ -13,8 +14,14 @@ public class Table
         int[] nines = {9,18,27,36,45,54,63,72,81,90};
         int[] tens = {10,20,30,40,50,60,70,80,90,100};
 
+        // Creates 2D array with all the 1d arrays as rows and the index values as columns
         int[][] table = { ones, twos, threes, fours, fives, sixes, sevens, eights, nines, tens };
 
+
+        // Nested loop to iterate through the table and then iterate through the 1d array
+        // System.outs the lines to have a traditional times table structure
+        // Last System.out prints a new line between arrays creating the rows
+        // \t is an Escape Sequence that performs a tab stop, creates equal spacing in the table
         for ( int i = 0; i < table.length; i++) {
             for ( int x = 0; x < table[i].length; x++){
                 System.out.print(table[i][x] + "\t");
